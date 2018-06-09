3ebbd0a7aa5f0734fb07e68fc734ee1bb261670f
snowjak88 <sjnowak88@gmail.com>
2018-06-09 16:44:47 -0400

    Added utility method: integration for Triplets

--- rays/core/src/main/java/org/snowjak/rays/Util.java
+++ rays/core/src/main/java/org/snowjak/rays/Util.java
@@ -1,7 +1,10 @@
 package org.snowjak.rays;
 
+import java.util.function.DoubleFunction;
 import java.util.function.DoubleUnaryOperator;
 import java.util.stream.DoubleStream;
+
+import org.snowjak.rays.geometry.util.Triplet;
 
 /**
  * Various helper methods too trivial to merit their own container classes.
@@ -41,6 +44,32 @@ public class Util {
 				.iterate(intervalStart + intervalStep / 2d, d -> d <= intervalEnd - intervalStep / 2d,
 						d -> d + intervalStep)
 				.map(d -> f.applyAsDouble(d)).reduce(0d, (d1, d2) -> d1 + d2) * intervalStep;
+	}
+	
+	/**
+	 * Analagous to {@link #integrate(double, double, int, DoubleFunction)},
+	 * allowing you to compute three integrals simultaneously (as components of a
+	 * Triplet).
+	 * 
+	 * @param intervalStart
+	 *            integration interval start point
+	 * @param intervalEnd
+	 *            integration interval end point
+	 * @param intervalCount
+	 *            number of slices the integration interval is divided into
+	 * @param f
+	 *            the function to integrate
+	 * @return the definite integral of <code>f</code> across the specified interval
+	 */
+	public static Triplet integrateTriplet(double intervalStart, double intervalEnd, int intervalCount,
+			DoubleFunction<Triplet> f) {
+		
+		final DoubleUnaryOperator xF = (x) -> f.apply(x).get(0), yF = (y) -> f.apply(y).get(1),
+				zF = (z) -> f.apply(z).get(2);
+		
+		return new Triplet(integrate(intervalStart, intervalEnd, intervalCount, xF),
+				integrate(intervalStart, intervalEnd, intervalCount, yF),
+				integrate(intervalStart, intervalEnd, intervalCount, zF));
 	}
 	
 }
--- rays/core/src/test/java/org/snowjak/rays/UtilTest.java
+++ rays/core/src/test/java/org/snowjak/rays/UtilTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.apache.commons.math3.util.FastMath;
 import org.junit.Test;
+import org.snowjak.rays.geometry.util.Triplet;
 
 public class UtilTest {
 	
@@ -14,9 +15,28 @@ public class UtilTest {
 	}
 	
 	@Test
-	public void test_linear() {
+	public void test_integrateLinear() {
 		
 		assertEquals(0.5d, Util.integrate(0d, 1d, 1024, (x) -> x), 0.00001);
+	}
+	
+	@Test
+	public void test_integrateTripletSine() {
+		
+		final var t = Util.integrateTriplet(0d, FastMath.PI / 2d, 1024,
+				(x) -> new Triplet(FastMath.sin(x), FastMath.cos(x), FastMath.sin(x)));
+		assertEquals(1d, t.get(0), 0.00001);
+		assertEquals(1d, t.get(1), 0.00001);
+		assertEquals(1d, t.get(2), 0.00001);
+	}
+	
+	@Test
+	public void test_integrateTripletLinear() {
+		
+		final var t = Util.integrateTriplet(0d, 1d, 1024, (x) -> new Triplet(x, -x, x));
+		assertEquals(0.5d, t.get(0), 0.00001);
+		assertEquals(-0.5d, t.get(1), 0.00001);
+		assertEquals(0.5d, t.get(2), 0.00001);
 	}
 	
 }
